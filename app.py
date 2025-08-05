9bb25b439221ef39b503c9d1c95367b032bff410
DanielTTrading <dv@ttrading.co>
2025-08-05 09:22:21 -0500

    enlace solo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -10,15 +11,14 @@ links = {
     "2025-08-14": "https://youtube.com/live/BEP9XNzL3n4?feature=share"   # 14 de agosto 2025
 }
 
-@app.route("/tactical_investor_week")
-def tactical_investor_week():
+@app.route("/")
+def redirect_to_event():
     today = datetime.today().strftime('%Y-%m-%d')  # Fecha actual
+    # Si la fecha no está en el diccionario, usa siempre el enlace del 12 de agosto
     url = links.get(today, links["2025-08-12"])
-  # Si no está en el diccionario, redirige a YouTube genérico
     return redirect(url)
 
 if __name__ == "__main__":
-    # Puerto que Railway usará, si no existe usa 5000
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
+
